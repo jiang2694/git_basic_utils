5a676cba30cc329d4188a97ad2f10800f9283518
Yiyi Wang <eewang1145@gmail.com>
2022-06-02 23:14:53 +0000

    Updated the add function

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 function product(n1, n2) {
   return n1, n2;
 }
-console.log(product(1,2));
+console.log(product(1, 2));
 
 function divide(n1, n2) {
   return n1 / n2;
@@ -9,4 +9,8 @@ function divide(n1, n2) {
 
 function power(n1, n1) {
   return Math.pow(n1, n2);
+}
+
+function add(n1, n2) {
+  return n1 + n2;
 }
